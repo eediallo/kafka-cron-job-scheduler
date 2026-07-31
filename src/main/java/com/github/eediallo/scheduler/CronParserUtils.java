7314d2c25bea3dd1363d5a445ee68add406f50bf
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-31 19:48:29 +0100

    add CronParserUtils class with method "toQuartzCron" to convert standard cron to quartz cron

---
+++ src/main/java/com/github/eediallo/scheduler/CronParserUtils.java
@@ -0,0 +1,23 @@
+package com.github.eediallo.scheduler;
+
+public class CronParserUtils {
+    public static String toQuartzCron(String standardCron) {
+        String[] parts = standardCron.split("\\s+");
+
+        if (parts.length == 5) {
+            String dom = parts[2];
+            String dow = parts[4];
+
+            if (dom.equals("*") && dow.equals("*")) {
+                dow = "?";
+            } else if (!dom.equals("*") && dow.equals("*")) {
+                dow = "?";
+            } else if (dom.equals("*") && !dow.equals("*")) {
+                dom = "?";
+            }
+
+            return String.format("0 %s %s %s %s %s", parts[0], parts[1], dom, parts[3], dow);
+        }
+        return standardCron;
+    }
+}
