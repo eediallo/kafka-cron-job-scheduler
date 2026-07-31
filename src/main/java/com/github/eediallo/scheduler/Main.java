cb55dc6f8a1bfac8323d9ca683b1588b998ca227
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-31 20:18:00 +0100

    add CronSchedulerService and refactor main

--- src/main/java/com/github/eediallo/scheduler/Main.java
+++ src/main/java/com/github/eediallo/scheduler/Main.java
@@ -1,53 +1,26 @@
 package com.github.eediallo.scheduler;
 
-import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.SchedulerException;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String filePath = args.length > 0 ? args[0] : "crontab.txt";
-        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            int lineNumber = 0;
-
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-
-                // skip empty lines or comments
-                if (line.isEmpty() || line.startsWith("#")) {
-                    lineNumber++;
-                    continue;
-                }
-
-                // Job Definition
-                JobDetail job = JobBuilder.newJob(CronJob.class)
-                        .withIdentity("job_" + lineNumber)
-                        .usingJobData("lineNumber", lineNumber)
-                        .build();
-
-                // Trigger Definition
-                Trigger trigger = TriggerBuilder.newTrigger()
-                        .withIdentity("trigger_" + lineNumber)
-                        .withSchedule(CronScheduleBuilder.cronSchedule(CronParserUtils.toQuartzCron(line)))
-                        .build();
-
-                // Schedule job
-                scheduler.scheduleJob(job, trigger);
-                System.out.println("Scheduled Job " + lineNumber + " with schedule: " + line);
-                lineNumber++;
-
-            }
-            scheduler.start();
+        try {
+            CronSchedulerService schedulerService = new CronSchedulerService();
+            schedulerService.loadAndScheduleJobs(filePath);
+            schedulerService.start();
             System.out.println("Cron Scheduler Running... Ctrl + c to exit");
         } catch (IOException e) {
-            System.err.println("Could not read crontab file: " + e.getMessage());
+            System.err.println("File Error: Could not read crontab file at '" + filePath + "' ." + e.getMessage());
+            System.exit(1);
+        } catch (SchedulerException e) {
+            System.err.println("Scheduler Error: Quartz engine failed to start. " + e.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Unexpected Error: " + e.getMessage());
             System.exit(1);
         }
     }
